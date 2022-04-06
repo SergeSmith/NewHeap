175804602684310822b1b5e82d6dce1c0177b4ef
SergeSmith <ssmithdeveloper@gmail.com>
2022-04-06 13:07:40 +0300

    коментарии

--- app/src/main/java/com/ssmithdeveloper/heap/MainActivity.kt
+++ app/src/main/java/com/ssmithdeveloper/heap/MainActivity.kt
@@ -3,4 +3,5 @@ package com.ssmithdeveloper.heap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Класс MainActivity
 class MainActivity :AppCompatActivity(R.layout.activity_main)
