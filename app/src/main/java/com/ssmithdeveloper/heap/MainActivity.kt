1de18b14477079b6e5cd6dc36903e9f768660226
SergeSmith <ssmithdeveloper@gmail.com>
2022-04-06 22:12:45 +0300

    firt commit junior local

--- app/src/main/java/com/ssmithdeveloper/heap/MainActivity.kt
+++ app/src/main/java/com/ssmithdeveloper/heap/MainActivity.kt
@@ -4,4 +4,5 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // Класс MainActivity
+// junior local create
 class MainActivity :AppCompatActivity(R.layout.activity_main)
